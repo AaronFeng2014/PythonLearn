a88f9cb09ed11611a6184b43ea9fdd02c0b5893a
albert.feng <albert.feng@pkfare.com>
2017-09-05 20:03:27 +0800

    python学习：添加dictionary和tuple数据结构
    Signed-off-by: albert.feng <albert.feng@pkfare.com>

---
+++ foo/main/com/aaron/python/basic/DictionaryLearn.py
@@ -0,0 +1,18 @@
+"""
+Dictionary数据结构：
+
+    类似java中的map结构
+
+"""
+
+dic = {"name": "fenghaixin", "age": 22, "home": "深圳市"}
+
+# dictionary数据结构取值
+print(dic["home"])
+print(dic["name"])
+
+# 添加，修改Dictionary数据结构中的数据
+dic["school"] = "成都大学"
+
+print(dic["school"])
+print(dic)
---
+++ foo/main/com/aaron/python/basic/TupleLearn.py
@@ -0,0 +1,15 @@
+"""
+元组
+    元组中的元素不能被修改
+    元组使用小括号，列表使用中括号
+"""
+
+# 这就是一个元组的创建
+resultTuple = (1, "fenghaixin", "ranran")
+
+print(resultTuple[2])
+
+# 元组只有一个元素时，后面必追加一个逗号
+resultTuple = (2,)
+
+print(resultTuple)
